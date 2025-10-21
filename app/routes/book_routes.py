7cfd95888b8d7c646254c5b74b532af2284fda9c
Maya Zakharova <mayzakharova@gmail.com>
2025-10-21 00:54:29 -0500

    finish part 3

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,7 +1,44 @@
-from flask import Blueprint, make_response, abort
+from flask import Blueprint, make_response, abort, request
+from app.models.book import Book
+from ..db import db
 # from app.models.book import books
 
 books_bp = Blueprint("book_bp", __name__, url_prefix="/books")
+
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+
+    return response, 201
+
+@books_bp.get("")
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+    # books = db.session.execute(query).scalars()
+
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
 
 # @books_bp.get("")
 # def get_all_books():
